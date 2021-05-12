b2fbd6fb257450246895a0a69295e9eb8ba4206f
josephcacsire98 <jcacsire10@gmail.com>
2021-05-12 14:03:14 -0400

    Final Project MWD 110 components

---
+++ main.js
@@ -0,0 +1,2 @@
+document.getElementsByTagName('body')[0].innerText = "This is my first paragraph with Java and I would like to writes about my experiences using it. This language is a bit complex and I'm quite enjoying it.";
+
